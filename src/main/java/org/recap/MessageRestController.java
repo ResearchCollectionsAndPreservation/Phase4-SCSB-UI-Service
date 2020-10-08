9e1b11631a79d8cf618e9597eaaabae02e37465e
mosesj <moses.joel@htcindia.com>
2020-10-08 13:57:40 +0530

    Adding Message controller

--- src/main/java/org/recap/MessageRestController.java
+++ src/main/java/org/recap/MessageRestController.java
@@ -1,14 +1,15 @@
-package org.recap.controller;
+package org.recap;
 
 import org.json.JSONObject;
 import org.recap.util.PropertyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;import java.util.Map;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RefreshScope
 @RestController
